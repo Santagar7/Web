4af7c33c94526c626728b5be6fe76a3c05fee26d
Santagar <assassins32791@gmail.com>
2022-06-16 14:45:50 +0300

    Lab_4

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import '../css/App.css';
+import { Routes,BrowserRouter,Route } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
+import SignUpPage from "./Pages/SignUpPage";
+import BudgetsPage from "./Pages/BudgetsPage";
+import MenuPage from "./Pages/MenuPage";
+import { createContext, useContext } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+          <Routes>
+              <Route exact path='/' element={<LoginPage/>}/>
+              <Route path='/signup' element={<SignUpPage/>}/>
+              <Route path='/budgets' element={<BudgetsPage/>}/>
+              <Route path='/menu/:index' element={<MenuPage/>}/>
+          </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
